54100d7a23610eca45d9e69ff5110556149ff814
takutonakagawa <t-nakagawa@willpride.co.jp>
2021-06-03 10:05:48 +0900

    'form'

--- join/index.php
+++ join/index.php
@@ -1,3 +1,30 @@
+<?php
+if(!empty($_POST)){
+	if($_POST['name'] === ''){
+		$error ['name'] = 'blank';
+	}
+
+	if($_POST['email'] === ''){
+		$error ['email'] = 'blank';
+	}
+
+	if(strlen($_POST['password']) < 4){
+		$error ['password'] = 'length';
+	}
+
+	if($_POST['password'] === ''){
+		$error ['password'] = 'blank';
+	}
+
+	if(empty($error)){
+	header('Location: check.php');
+	exit();
+	}
+
+}
+  
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -20,15 +47,40 @@
 	<dl>
 		<dt>ニックネーム<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="name" size="35" maxlength="255" value="" />
+        	<input type="text" name="name" size="35" maxlength="255" value="<?php print
+			(htmlspecialchars($_POST['name'], ENT_QUOTES )); ?>" />
+			<?php if($error['name'] === 'blank'): ?>
+				<p class="error">*ニックネームを入力してください</p>
+			<?php endif ?>
 		</dd>
+
+
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="email" size="35" maxlength="255" value="" />
+        	<input type="text" name="email" size="35" maxlength="255" value="<?php print
+			(htmlspecialchars($_POST['email'], ENT_QUOTES )); ?>" />
+			<?php if($error['email'] === 'blank'): ?>
+				<p class="error">*メールアドレスを入力してください</p>
+			<?php endif ?>
+
+
+
+			
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
-        	<input type="password" name="password" size="10" maxlength="20" value="" />
+        	<input type="password" name="password" size="10" maxlength="20" value="<?php print
+			(htmlspecialchars($_POST['password'], ENT_QUOTES )); ?>" />
+			<?php if($error['password'] === 'blank'): ?>
+				<p class="error">*パスワードを入力してください</p>
+			<?php endif ?>
+
+			<?php if($error['password'] === 'length'): ?>
+				<p class="error">*パスワードは4文字以上入力してください</p>
+			<?php endif ?>
         </dd>
+
+
+		
 		<dt>写真など</dt>
 		<dd>
         	<input type="file" name="image" size="35" value="test"  />
